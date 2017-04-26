433817b78aeda79a5cf5045abb68b5ae81be0281
Fabian Grimminger <grimmingerapps@gmail.com>
2017-04-26 15:40:05 +0200

    Test for Member

---
+++ public/src/app/event/member.spec.ts
@@ -0,0 +1,17 @@
+import {Member} from './Member';
+
+describe('Member', () =>{
+
+    it('#Email', ()=>{
+        let member:Member = new Member("abc@def.gh","asf165f");
+        expect(member.equals(member)).toBeTruthy();
+        let member2:Member = new Member("abc@def.gh","asf165f");
+        expect(member.equals(member2)).toBeTruthy();
+        member.email="neue@mai.com";
+        expect(member.equals(member2)).toBeFalsy();
+        member.email=member2.email;
+        member.id="neue";
+        expect(member.equals(member2)).toBeFalsy();
+    });
+
+});
