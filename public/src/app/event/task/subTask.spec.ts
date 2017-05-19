e2cb0fa78899f97b3a30bdf47ab3bfa6998ba5ac
Basti <inkeyes98@googlemail.com>
2017-05-19 13:08:05 +0200

    added tests for subtask

--- public/src/app/event/task/subTask.spec.ts
+++ public/src/app/event/task/subTask.spec.ts
@@ -1,11 +1,39 @@
 import {SubTask} from './subTask';
 
 describe('SubTask', () =>{
+    let subTask: SubTask;
+
+    beforeEach(() =>
+    {
+        subTask = new SubTask("Title");
+    });
+
     it('#title', () =>{
-        let subTask: SubTask = new SubTask("title");
-        expect(subTask.getTitle()).toBe("title");
+        expect(subTask.getTitle()).toBe("Title");
         let t: string = "newTitle";
         subTask.setTitle("newTitle");
         expect(subTask.getTitle()).toBe(t);
+    });
+
+    it('#Key', () => 
+    {
+        subTask.setKey("2rqfgh4eavz34bv7");
+        expect(subTask.getKey()).toBe("2rqfgh4eavz34bv7");
+        subTask.setKey("rtvgbhngt567zhugvfrtgzh");
+        expect(subTask.getKey()).toBe("rtvgbhngt567zhugvfrtgzh");
+    });
+
+    it('#Done', () =>
+    {
+        subTask.setDone(true);
+        expect(subTask.getDone()).toBeTruthy();
+        subTask.setDone(false);
+        expect(subTask.getDone()).toBeFalsy();
+    });
+
+    it('#EventKey', () =>
+    {
+        subTask.eventkey = "unrvwubvzeua3wr7824gh";
+        expect(subTask.getEventKey()).toBe("unrvwubvzeua3wr7824gh");
     });
 });
